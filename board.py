915f3923fc868ae9d58acca535b11c1e97e9f4f2
Jane Cakemaster <janecakemaster@gmail.com>
2013-10-23 03:20:08 -0400

    board done..?

--- board.py
+++ board.py
@@ -8,13 +8,15 @@ from space import Space
 class Board:
 
     def __init__(self, filename):
-        self.walls, self.boxes, self.goals, self.spaces, self.rows = ([] for i in range(5))
+        self.walls, self.boxes, self.goals, self.spaces, self.rows = (
+            [] for i in range(5))
         with open(filename, 'r') as f:
             read_data = f.read()
             lines = read_data.split('\n')
-            lines.pop(0)
+            self.height = lines.pop(0)
             x = 0
             y = 0
+            self.width = 0
             player_found = False
             for line in lines:
                 row = []
@@ -48,11 +50,23 @@ class Board:
                         row.append(s)
                         self.spaces.append(s)
                     x += 1
+                if len(row) > self.width:
+                    self.width = len(row)
                 self.rows.append(row)
                 y += 1
             if player_found == False:
                 self.rows = []
                 print "no player found on this board"
+
+    def at(self, x, y):
+        return self.rows[x][y]
+
+    def moves_available(self):
+
+        return
+
+    def moves(self):
+        return
 
     def __str__(self):
         text = ''
@@ -62,8 +76,3 @@ class Board:
             text += '\n'
         return text
 
-    def moves_available(self):
-        return
-
-    def moves(self):
-        return
